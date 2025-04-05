a7992353d7d63a47a85bbfd9644a0e2cb9181ee8
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-04-05 00:38:06 -0500

    Time: 1 ms (12.27%) | Memory: 11.2 MB (18.52%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.c
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.c
@@ -6,6 +6,34 @@
  * };
  */
 struct ListNode* deleteDuplicates(struct ListNode* head) {
+    struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
+    dummy->next = NULL;
+    struct ListNode* tail = dummy;
+    struct ListNode* current = head;
+    while(current!=NULL){
+        if(current->next!=NULL && current->val == current->next->val){
+            struct ListNode* newNode = malloc(sizeof(struct ListNode));
+            newNode->val = current->val;
+            newNode->next = NULL;
+            tail->next = newNode;
+            tail = newNode;
+            while (current->next != NULL && current->val == current->next->val) {
+                current = current->next;
+            }
+            current = current->next;
+        }
+        else{
+            struct ListNode* newNode = malloc(sizeof(struct ListNode));
+            newNode->val = current->val;
+            newNode->next = NULL;
+            tail->next = newNode;
+            tail = newNode;
+            current = current->next;
+        } 
+    }
+    return dummy->next;
+}
+    /*
     struct ListNode* current = head;
     while(current!=NULL){
         if(current->next != NULL && current->val == current->next->val){
@@ -18,3 +46,4 @@ struct ListNode* deleteDuplicates(struct ListNode* head) {
     }
     return head;
 }
+*/
