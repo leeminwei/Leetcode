ac65c296fae97bc200604fb72b3f9b455f12aca2
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-04-14 19:51:24 -0500

    Time: 0 ms (100.00%) | Memory: 7.9 MB (31.18%) - LeetSync

---
+++ 476-number-complement/number-complement.c
@@ -0,0 +1,25 @@
+int findComplement(int num) {
+    char* arr = (char*)malloc(1*sizeof(char));
+    int size = 0;
+    int count = 0;
+    char number;
+    while(num>=1){
+        arr = (char*)realloc(arr,(size+1)*sizeof(char));
+        arr[size] = num%2 +'0';
+        if(arr[size]=='0')
+            arr[size]='1';
+        else
+            arr[size]='0';
+        size++;
+        num = num/2;
+    }
+    /*
+    for (int i = 0; i < size; i++) {
+        arr[i] = ~arr[i];
+    }
+    */
+    for(int i=size-1 ; i>=0 ; i--){
+        count = count*2 + (arr[i]-'0');
+    }
+    return count;
+}
