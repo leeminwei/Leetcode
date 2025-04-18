a50c1debf1cf45cb0404173234cd3ada4572791b
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-04-17 19:26:41 -0500

    Time: 49 ms (24.41%) | Memory: 14.4 MB (98.03%) - LeetSync

---
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.c
@@ -0,0 +1,18 @@
+int countConsistentStrings(char * allowed, char ** words, int wordsSize){
+    int allow_mask = 0;
+    for(int i=0 ; i<strlen(allowed) ; i++){
+        allow_mask |= 1<<abs(allowed[i]-'a');
+        //printf("%d\n",allow_mask);
+    }
+    int count = 0;
+    for(int i=0 ; i<wordsSize ; i++){
+        int words_mask =0;
+        for(int j=0 ; j<strlen(words[i]) ; j++){
+            words_mask |= 1<<abs(words[i][j]-'a');
+        }
+        if((words_mask & ~allow_mask) == 0){
+                count++;
+            }
+    }
+    return count;
+}
