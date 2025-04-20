7fda95348ae8eec1bae172b0ec646d1cb6a594e1
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-04-20 16:22:44 -0500

    Time: 0 ms (100.00%) | Memory: 10.4 MB (80.35%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.c
@@ -0,0 +1,26 @@
+int hash_table(char s){
+    switch(s)
+    {
+        case 'I' : return 1;
+        case 'V' : return 5;
+        case 'X' : return 10;
+        case 'L' : return 50;
+        case 'C' : return 100;
+        case 'D' : return 500;
+        case 'M' : return 1000;
+        default : return 0;
+    }
+}
+
+int romanToInt(char* s) {
+    int count = 0;
+    for(int i=0 ; i<strlen(s)-1 ; i++){
+        if(hash_table(s[i]) < hash_table(s[i+1])){
+            count = count - hash_table(s[i]);
+        }
+        else
+            count = count + hash_table(s[i]);
+    }
+    count = count + hash_table(s[strlen(s)-1]);
+    return count;
+}
