d7dee6000745af129ab1bfa15509d66c19478aad
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-04-27 23:16:43 -0500

    Time: 36 ms (16.67%) | Memory: 9.4 MB (50.00%) - LeetSync

--- 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.c
+++ 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.c
@@ -1,6 +1,7 @@
+//用bitmask來代表words[i]的結果
 int bitmask(char* s) {
     int size = strlen(s);
-    int arr = 0; // 不是指標，只是一個整數
+    int arr = 0;
     for (int i = 0; i < size; i++) {
         arr |= 1 << (s[i] - 'a');
     }
@@ -10,7 +11,7 @@ int bitmask(char* s) {
 int similarPairs(char** words, int wordsSize) {
     int count = 0;
     for (int i = 0; i < wordsSize; i++) {
-        for (int j = i + 1; j < wordsSize; j++) { // 要兩層迴圈
+        for (int j = i + 1; j < wordsSize; j++) {
             if (bitmask(words[i]) == bitmask(words[j])) {
                 count++;
             }
