ff72e149071f5e9370e1ba3721f5de11c4bc8975
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-04-28 22:51:50 -0500

    Time: 1402 ms (5.13%) | Memory: 13.6 MB (51.28%) - LeetSync

---
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.c
@@ -0,0 +1,22 @@
+int bitmask(char* s){
+    int size = strlen(s);
+    int arr = 0;
+    for(int i=0 ; i<size ; i++){
+        arr |= 1<<(s[i]-'a');
+    }
+    return arr;
+}
+int maxProduct(char** words, int wordsSize) {
+    int max_len = 0;
+    int len = 0;
+    for(int i=0 ; i<wordsSize ; i++){
+        for(int j=i+1 ; j<wordsSize ; j++){
+            if((bitmask(words[i])&bitmask(words[j]))==0){
+                len = strlen(words[i])*strlen(words[j]);
+            }
+            if(len>max_len)
+                max_len = len;
+        }
+    }
+    return max_len;
+}
