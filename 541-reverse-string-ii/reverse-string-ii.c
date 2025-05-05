4af5853b1b721fd8df5c5358f6c79390fd939a90
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-05-05 17:35:00 -0500

    Time: 0 ms (100.00%) | Memory: 8.6 MB (4.10%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.c
+++ 541-reverse-string-ii/reverse-string-ii.c
@@ -2,33 +2,29 @@ char* reverse(char* s, int start, int end) {
     int size = end - start;
     char* arr = malloc(size * sizeof(char));
     int number = 0;
-
     for (int i = end - 1; i >= start; i--) {
         arr[number++] = s[i];
     }
-
     for (int i = 0; i < size; i++) {
         s[start + i] = arr[i];
     }
-
     free(arr);
     return s;
 }
-
 char* reverseStr(char* s, int k) {
     int size = strlen(s);
-    int i = 0;
-    int end = 0;
-
-    while (i < size) {
-        if (i + k < size)
-            end = i + k;
-        else
-            end = size;
-
-        s = reverse(s, i, end);
-        i += 2 * k;
+    int point2 = 2*k;
+    int point = k;
+    int index = 0;
+    while(point2<=size){
+        s = reverse(s,index,point);
+        index += 2*k;
+        point2 += 2*k;
+        point += 2*k;
     }
-
+    int right = point;
+    if (right > size)
+        right = size;
+    s = reverse(s,index,right);
     return s;
 }
