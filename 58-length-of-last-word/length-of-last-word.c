883b0dc7c0d30179b1c7d8f4e925f1a79f145af5
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-05-09 23:59:08 -0500

    Time: 0 ms (100.00%) | Memory: 8.2 MB (13.01%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.c
+++ 58-length-of-last-word/length-of-last-word.c
@@ -1,8 +1,6 @@
 int lengthOfLastWord(char* s) {
     char* token;
     int size = strlen(s);
-    char* arr = malloc(size*sizeof(char));
-    int index = 0;
     token = strtok(s," ");
     int token_size=0;
     while(token!=NULL){
@@ -10,10 +8,5 @@ int lengthOfLastWord(char* s) {
         token_size = strlen(token);
         token = strtok(NULL," ");
     }
-    /*
-    for(int i=0 ; i<token_size ; i++){
-        arr[index++] = token[i];
-    }
-    */
     return token_size;
 }
