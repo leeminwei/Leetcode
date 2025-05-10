2a49c3b8344f9c3398f50f6744d429f1dbee12aa
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-05-10 16:03:02 -0500

    Time: 7 ms (17.86%) | Memory: 8.6 MB (39.74%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.c
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.c
@@ -1,36 +1,41 @@
 char* reverseWords(char* s) {
+    char* token;
     char* s_copy = strdup(s);
-    if (s_copy == NULL) return NULL;
-
     int len = strlen(s_copy);
-    char* arr = malloc((len + 1) * sizeof(char));
-    if (arr == NULL) {
-        free(s_copy);
+    printf("%d\n",len);
+    char* arr = malloc((len + 1)*sizeof(char));
+    if (s_copy == NULL) {
+        perror("strdup");
         return NULL;
     }
-
-    char* token = strtok(s_copy, " ");
+    token = strtok(s_copy," ");
     if (token == NULL) {
         arr[0] = '\0';
         free(s_copy);
         return arr;
     }
-
-    int position = len;
-    while (token != NULL) {
-        int size = strlen(token);
-        position -= size;
-        strncpy(arr + position, token, size);
-
-        token = strtok(NULL, " ");
-        if (token != NULL) {
-            position--;
-            arr[position] = ' ';
+    int size = strlen(token);
+    int position = len-size;
+    strncpy(arr+position,token,size);
+    arr[len] = '\0';
+    while(token!=NULL){
+        if(position<=0)
+            break;
+        position--;
+        arr[position] = ' ';
+        token = strtok(NULL," ");
+        if (token != NULL) { // 檢查 token 是否為 NULL
+            size = strlen(token);
+            position -= size;
+            printf("%d\n",position);
+            strncpy(arr+position,token,size);
         }
     }
-
-    memmove(arr, arr + position, len - position);
-    arr[len - position] = '\0';
+    while (!isalnum(arr[0])) {
+        memmove(arr, arr + 1, strlen(arr));
+    }
+    int shift = strlen(arr);
+    arr[shift] = '\0';
     free(s_copy);
     return arr;
 }
