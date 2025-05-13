e97822239b321bdb3bbfd4ffd114998e3f215733
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-05-13 16:05:26 -0500

    Time: 438 ms (14.44%) | Memory: 17.2 MB (66.24%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.c
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.c
@@ -8,16 +8,31 @@
 struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
     struct ListNode *current = headA;
     struct ListNode *current2 = headB;
-    while(current!=NULL){
+    while(current!=NULL || current2!=NULL){
+        if(current==NULL)
+            current = headA;
+        if(current2==NULL)
+            current2 = headB;
+        if(current==current2)
+            return current;
+        current = current->next;
+        current2 = current2->next;
+    }
+    return 0;
+}
+/*
+ while(current!=NULL){
         while(current2!=NULL){
             if(current == current2){
                 return current;
             }
             else
                current2 = current2->next;
+               printf("current2 = %d",current2);
         }
         current = current->next;
+        printf("current = %d",current);
         current2 = headB;  //要在讓headB從頭驗證一次
     }
     return NULL;
-}
+    */
