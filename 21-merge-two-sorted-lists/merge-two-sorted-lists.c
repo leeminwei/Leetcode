a2da56403e58ddb1d648af74cab4a25df7364407
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-05-13 14:59:35 -0500

    Time: 0 ms (100.00%) | Memory: 11 MB (7.92%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.c
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.c
@@ -7,10 +7,40 @@
  */
 
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
-    struct ListNode dummy;
+    struct ListNode* tail = (struct ListNode*)malloc(sizeof(struct ListNode));
+    tail->next = NULL;
+    struct ListNode* dummy = tail;
+    while(list1!=NULL || list2!=NULL){
+        if(list1!=NULL&&list2!=NULL){
+            if(list1->val>list2->val){
+                tail->next = list2;
+                tail = tail->next;
+                list2 = list2->next;
+            }
+            else{
+                tail->next = list1;
+                tail = tail->next;
+                list1 = list1->next;
+            }
+        }
+        else if(list1!=NULL){
+            tail->next = list1;
+            tail = tail->next;
+            list1 = list1->next;
+        }
+        else{
+            tail->next = list2;
+            tail = tail->next;
+            list2 = list2->next;
+        }
+    }
+    return dummy->next;
+}
+/*
+struct ListNode dummy;
     struct ListNode* tail = &dummy;
     dummy.next = NULL;
-    while (list1 && list2) {
+    while (list1!=NULL && list2!=NULL) {
         if(list1 -> val < list2 -> val){
             tail -> next = list1; 
             list1 = list1 -> next;
@@ -29,4 +59,4 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
 
     // 回傳合併後 linked list 的頭（跳過 dummy）
     return dummy.next;
-}
+*/
