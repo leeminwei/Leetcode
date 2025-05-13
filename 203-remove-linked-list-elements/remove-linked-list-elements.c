726f1d29d79735818684ecd9892102555ec84b9e
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-05-13 16:15:27 -0500

    Time: 0 ms (100.00%) | Memory: 12.4 MB (80.60%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.c
+++ 203-remove-linked-list-elements/remove-linked-list-elements.c
@@ -6,7 +6,24 @@
  * };
  */
 struct ListNode* removeElements(struct ListNode* head, int val) {
-    struct ListNode* newHead = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* tail = (struct ListNode*)malloc(sizeof(struct ListNode));
+    tail->next = NULL;
+    struct ListNode* dummy = tail;
+    struct ListNode* current = head;
+    while(current!=NULL){
+        if(current->val==val)
+            current = current->next;
+        else{
+            tail->next = current;
+            tail = tail->next;
+            current = current->next;
+        }
+    }
+    tail->next = NULL;
+    return dummy->next;
+}
+/*
+struct ListNode* newHead = (struct ListNode*)malloc(sizeof(struct ListNode));
     newHead->next = NULL;
     struct ListNode* tail = newHead;
     struct ListNode* current = head;
@@ -21,4 +38,4 @@ struct ListNode* removeElements(struct ListNode* head, int val) {
     //struct ListNode* result = newHead->next;
     //free(newHead); 
     return newHead->next;
-}
+*/
