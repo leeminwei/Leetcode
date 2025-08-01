36c521aaed9bb6494bc171fd8fe426f838e1644b
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-08-02 01:17:58 +0800

    Time: 0 ms (100.00%) | Memory: 8.6 MB (90.58%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.c
@@ -0,0 +1,33 @@
+int myAtoi(char* s) {
+    int size = strlen(s);
+    long long int sum = 0;
+    int check = 0;
+    int sign = 1;
+    for (int i=0 ; i<size ; i++) {
+        if ((s[i] - '0') >= 0 && (s[i] - '0') <= 9) {
+            sum = sum * 10 + (s[i] - '0');
+            check = 1;
+            if (sign * sum >= INT_MAX) {
+                return INT_MAX;
+            }
+            else if (sign * sum <= INT_MIN) {
+                return INT_MIN;
+            }
+        }
+        else if ((s[i] - '0') > 9 || (s[i] - '0') < 0) {
+            if (s[i] == ' ' && check == 0) 
+                continue;
+            else if (s[i] == '-' && check == 0) {
+                sign = -1;
+                check = 1;
+            }
+            else if (s[i] == '+' && check == 0) {
+                sign = 1;
+                check = 1;
+            }
+            else
+                break;
+        }
+    }
+    return sign * sum;
+}
