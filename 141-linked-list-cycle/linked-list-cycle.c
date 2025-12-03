240f0564d44fc922e86769743a4ec2f34445e53d
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-12-03 16:19:34 +0800

    Time: 14 ms (17.59%) | Memory: 11.1 MB (78.24%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.c
+++ 141-linked-list-cycle/linked-list-cycle.c
@@ -6,12 +6,13 @@
  * };
  */
 bool hasCycle(struct ListNode *head) {
-    struct ListNode* fast = head;
-    struct ListNode* slow = head;
-    while(fast!=NULL && fast->next!=NULL){
+    typedef struct ListNode ListNode;
+    ListNode* fast = head;
+    ListNode* slow = head;
+    while (fast != NULL && fast->next != NULL) {
         fast = fast->next->next;
         slow = slow->next;
-        if(fast == slow){
+        if (fast == slow) {
             return true;
         }
     }
