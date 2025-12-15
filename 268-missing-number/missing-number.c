52b93ed74af0d17175bb91c3b33459c774a20b2d
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-12-15 16:10:02 +0800

    Time: 0 ms (100.00%) | Memory: 8.9 MB (29.98%) - LeetSync

--- 268-missing-number/missing-number.c
+++ 268-missing-number/missing-number.c
@@ -1,20 +1,10 @@
 int missingNumber(int* nums, int numsSize) {
-    int number = numsSize+1;
-    int count = 0;
-    int clock = 0;
-    int* arr = (int*)malloc((numsSize + 1) * sizeof(int));
-    for(int i=0; i<numsSize+1 ; i++){
-        arr[i] = i;
+    int xor = 0;
+    for (int i=0 ; i<=numsSize ; i++) {
+        xor ^= i;
     }
-    for(int i=0 ; i<number ; i++){
-        for(int j=0 ; j<numsSize ; j++){
-            if(arr[i] != nums[j]){
-                clock++;
-            }
-            if(clock == numsSize)
-                return arr[i];           
-        }
-        clock = 0;
+    for (int i=0 ; i<numsSize ; i++) {
+        xor ^= nums[i];
     }
-    return 0;
+    return xor;
 }
