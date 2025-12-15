9e61c6fdb9f20aa44c797b7dc3e2b4d7dd46dcbb
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-12-15 15:55:01 +0800

    Time: 1067 ms (5.06%) | Memory: 9.3 MB (68.95%) - LeetSync

--- 136-single-number/single-number.c
+++ 136-single-number/single-number.c
@@ -1,25 +1,14 @@
 int singleNumber(int* nums, int numsSize) {
-    int number = numsSize;
-    int *size = &number;
-    int freq[60001]={0};
-    for(int i=0 ; i<numsSize ; i++){
-        freq[nums[i]+30000]++;
-    }
-    for(int i=0 ; i<*size ; i++){
-        for(int j=i+1 ; j<*size ; j++){
-            if(nums[i]==nums[j]){
-                for(int k=j ; k<*size-1 ; k++){
-                    nums[k] = nums[k+1];
-                }
-                (*size)--;
-                j--;
+    for (int i=0 ; i<numsSize ; i++) {
+        int count = 0;
+        for (int j=0 ; j<numsSize ; j++) {
+            if (nums[i] == nums[j]) {
+                count++;
             }
         }
-    }
-    for(int i=0 ; i<*size ;i++){
-        printf("%d\n",freq[nums[i]+30000]);
-        if(freq[nums[i]+30000] == 1)
+        if (count == 1) {
             return nums[i];
+        }
     }
     return 0;
 }
