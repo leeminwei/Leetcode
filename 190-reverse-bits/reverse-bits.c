79283ef066867efa5ae1feb8dba46071f4363353
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-12-15 15:42:27 +0800

    Time: 5 ms (14.67%) | Memory: 8.2 MB (59.89%) - LeetSync

--- 190-reverse-bits/reverse-bits.c
+++ 190-reverse-bits/reverse-bits.c
@@ -1,14 +1,10 @@
 uint32_t reverseBits(uint32_t n) {
-    int answer = 0;
-    int bits[32];
-    for(int i = 0; i < 32; i++){
-        bits[i] = n%2;
-        n/=2;
-        printf("%d\n",bits[i]);
+    uint32_t num = 0;
+    for (int i=0 ; i<32 ; i++) {
+        uint32_t bit = n & 1;
+        n >>= 1;
+        num <<= 1;
+        num |= bit;
     }
-    for(int i = 0; i < 32; i++){
-        answer += bits[i] * pow(2, 31-i);
-    }
-
-    return answer;
+    return num;
 }
