baffad4f5bbf5bf32f01a4d7dbf09605c8fe5f90
Min Wei Li <114138019+leeminwei@users.noreply.github.com>
2025-12-15 15:26:28 +0800

    Time: 0 ms (100.00%) | Memory: 8.1 MB (64.95%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.c
+++ 191-number-of-1-bits/number-of-1-bits.c
@@ -1,31 +1,8 @@
 int hammingWeight(int n) {
-    int size = 0;
-    int number = 0;
     int count = 0;
-    int *array = NULL;
-    while(1){
-        array = (int*)realloc(array, (size + 1) * sizeof(int));
-        array[number] = n%2;
-        n = n/2;
-        number++;
-        size++;
-        if(n==0)
-            break;
-    }
-    //將array顛倒儲存在arr陣列中，arr為正確的二進制形式
-    int arr[size]={};
-    int record = 0;
-    for(int i=size ; i>0 ; i--){
-        arr[record] = array[i-1];
-        printf("%d",arr[record]);
-        record++;
-    }
-    //數二進制中有幾個1
-    for(int i=0 ; i<size ; i++){
-        if(array[i]==1)
-            count++;
-        else
-            continue;        
+    while (n != 0) {
+        n &= (n-1);
+        count++;
     }
     return count;
 }
